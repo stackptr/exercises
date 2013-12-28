1129ab3b29852c9304181ac4fae68fc5aa55e975
stackptr <corey@x64.co>
2013-12-28 15:57:42 -0500

    CodeEval: Trailing String

---
+++ CodeEval/32-trailing-string.js
@@ -0,0 +1,30 @@
+/* CodeEval challenge 32
+ * Trailing string
+ *
+ * Requirements:
+ *  - Program accepts as its first argument a path to a filename
+ *	- Each line of input file contains two strings, comma delimited
+ *	- Print 1 if the second string occurs at the end of the first string
+ *	- Print 0  otherwise
+ * 
+ */
+
+var fs  = require("fs");
+var inputFile = process.argv[2]
+
+fs.readFileSync(inputFile) 	// Returns contents of input file
+  .toString()				// Converts contents to String type
+  .split('\n')				// Data is now an array of rounds
+  .forEach(processInput);
+
+// Takes string of space delimited numbers, outputs lowest unique number
+function processInput(line){
+	if (line == "") return;
+
+	var input = line.split(',');
+	if(input[0].indexOf(input[1]) + input[1].length == input[0].length){
+		console.log(1);
+	} else {
+		console.log(0)
+	}
+}
