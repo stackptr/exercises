5d34e2c76493c7a91e2093a515569573c7305183
stackptr <corey@x64.co>
2013-12-30 09:58:08 -0500

    CodeEval: Decryption - substitution cipher implementation

--- CodeEval/61-decryption.js
+++ CodeEval/61-decryption.js
@@ -10,8 +10,39 @@
  * 
  */
 
+// Rotate an array
+function rotate(Arr, n){
+	if (Arr.length > 0){
+		for (var i = 0; i < n; i++){
+			Arr.unshift(Arr.pop());
+		}
+	}
+}
+
+// Decrypt substitution cipher
+//   msg is an array of decrypted letters
+//   key is an array that maps char[i] => code
+function decrypt(msg, key){
+	var result = [];
+	msg.forEach(function(v, i){
+		result[i] = key[parseInt(v, 10)];
+	});
+	return result;
+}
+
+// Print function
+function printMsg(msg){
+	for (var i = 0; i < msg.length; i++){
+		process.stdout.write(msg[i]);
+		if (i == 2 || i == 7 || i == 12 || i == 19 || i == 21 || i == 29 ){
+			process.stdout.write(' ');
+		}
+	}
+	process.stdout.write('\n');
+}
+
 var message = "012222 1114142503 0313012513 03141418192102 0113 2419182119021713 06131715070119";
-var key = "BHISOECRTMGWYVALUZDNFJKPQX";
+var key = "BHISOECRTMGWYVALUZDNFJKPQX".split('');
 
 // Key is 26 characters long and each character only appears once. Therefore,
 // a cipher is assumed, where the position of each letter correspond to
@@ -48,6 +79,18 @@ letters.forEach(function(v){
 	}
 });
 
-frequencies.forEach(function(v, i){
-	console.log(i + ": " + v);
-});
+for (var i = 0; i < 26; i++){
+	if (frequencies[i] === undefined){
+		frequencies[i] = 0;
+	}
+}
+
+// frequencies.forEach(function(v, i){
+// 	console.log(String.fromCharCode(65+i) + ": " + v);
+// });
+
+for (var i = 0; i < 26; i++){
+	console.log("Rotation: " + i);
+	rotate(key,1);
+	printMsg(decrypt(letters,key));
+}
